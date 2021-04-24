a8a4ae6ea373793aa1e8c665e6a6931fe99d2862
saichandrasekar <saichandrasekar@gmail.com>
2021-04-24 22:31:49 +0530

    initial changes

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Welcome to Data Structures and Alogrithms\n\nMenu::\n1. Run an insertion sort");
+	
+	do_insertion_sort();
+}
+
+fn do_insertion_sort() {
+
+	let input_array = [2,5,6,3,5];
+	
+	println!("{}",input_array[0]);
+}
